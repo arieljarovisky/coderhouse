f98b4de5e8a1352fe9110debfe810376fde06d9e
arieljarovisky <jaroviskyariel@gmail.com>
2022-06-06 20:04:23 -0300

    mayusculas

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+
+import './navbar.css'
+import Carrito from '../Carrito/Carrito';
+import { Link, NavLink } from 'react-router-dom'
+const navbar = () => {
+
+    
+    return (
+      <nav className="NavBar" >
+      <NavLink to='/' className='titulo'>
+        Tienda Online
+      </NavLink>
+      <div className="Categories">
+         
+          <NavLink to='/category/pantalones' className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Pantalones</NavLink>
+          <NavLink to='/category/remeras' className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Remeras</NavLink>
+          
+      </div>
+      <Carrito />
+  </nav>
+    )
+}
+
+export default navbar
